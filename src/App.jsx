7050e4312a6d0ac451bf5f38b6bee3050e0b3edc
Hasib Islam <hasib64dj@gmail.com>
2026-01-16 23:25:24 +0600

    I have completed the animation work for the home page hero section and another section today.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,253 @@
-import React from 'react'
+import React, { useState } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import 'remixicon/fonts/remixicon.css';
 
-function App() {
-  return (
-    <div>App</div>
-  )
+export default function App() {
+    let [showContent, setShowContent] = useState(false);
+
+    // First intro animation
+    useGSAP(() => {
+        const tl = gsap.timeline();
+
+        tl.to('.vi-mask-group', {
+            rotate: 10,
+            duration: 2,
+            ease: 'Power4.easeInOut',
+            transformOrigin: '50% 50%',
+        }).to('.vi-mask-group', {
+            scale: 10,
+            duration: 2,
+            delay: -1.8,
+            ease: 'Expo.easeInOut',
+            transformOrigin: '50% 50%',
+            opacity: 0,
+            onUpdate: function () {
+                if (this.progress() >= 0.9) {
+                    document.querySelector('.svg').remove();
+                    setShowContent(true);
+                    this.kill();
+                }
+            },
+        });
+    });
+
+    // Landing section animation
+    useGSAP(() => {
+        if (!showContent) return;
+
+        gsap.to('.main', {
+            scale: 1,
+            rotate: 0,
+            duration: 2,
+            delay: '-1',
+            ease: 'Expo.easeInOut',
+        });
+
+        gsap.to('.sky', {
+            scale: 1.1,
+            rotate: 0,
+            duration: 2,
+            delay: '-.8',
+            ease: 'Expo.easeInOut',
+        });
+
+        gsap.to('.bg', {
+            scale: 1.1,
+            rotate: 0,
+            duration: 2,
+            delay: '-.8',
+            ease: 'Expo.easeInOut',
+        });
+
+        gsap.to('.charater', {
+            scale: 1.4,
+            x: '-50%',
+            bottom: '-25%',
+            rotate: 0,
+            duration: 2,
+            delay: '-.8',
+            ease: 'Expo.easeInOut',
+        });
+
+        gsap.to('.text', {
+            scale: 1,
+            rotate: 0,
+            duration: 2,
+            delay: '-.8',
+            ease: 'Expo.easeInOut',
+        });
+
+        gsap.to('.logo18', {
+            scale: 1.2,
+            rotate: 0,
+            duration: 2,
+            delay: '-.8',
+            ease: 'Expo.easeInOut',
+        });
+
+        const main = document.querySelector('.main');
+
+        main?.addEventListener('mousemove', function (e) {
+            const XMove = (e.clientX / window.innerWidth - 0.5) * 40;
+
+            gsap.to('.main .text', {
+                x: `${XMove * 0.4}%`,
+            });
+            gsap.to('.sky', {
+                x: XMove,
+            });
+            gsap.to('.bg', {
+                x: XMove * 0.7,
+            });
+        });
+    }, [showContent]);
+
+    return (
+        <>
+            {/* Website intro part */}
+            <div className="svg flex items-center justify-center fixed top-0 left-0 z-50 w-full h-screen overflow-hidden bg-black">
+                <svg viewBox="0 0 800 600" preserveAspectRatio="xMidYMid slice">
+                    <defs>
+                        <mask id="viMask">
+                            <rect width="100%" height="100%" fill="black" />
+                            <g className="vi-mask-group">
+                                <text
+                                    x="50%"
+                                    y="50%"
+                                    fontSize="250px"
+                                    fontFamily="Arial black"
+                                    fill="white"
+                                    textAnchor="middle"
+                                    dominantBaseline="middle"
+                                >
+                                    VI
+                                </text>
+                            </g>
+                        </mask>
+                    </defs>
+                    <image
+                        href="./bg.png"
+                        width="100%"
+                        height="100%"
+                        preserveAspectRatio="xMidYMid slice"
+                        mask="url(#viMask)"
+                    />
+                </svg>
+            </div>
+
+            {/* Full landing part */}
+            {showContent && (
+                <div className="w-full overflow-hidden">
+                    <div className="main w-full scale-[1.7] rotate-[-10deg]">
+                        {/* Landing top part */}
+                        <div className="landing w-full h-screen bg-black">
+                            <div className="navbar absolute z-10 top-0 left-0 w-full py-16 px-16">
+                                <div className="logo flex gap-6">
+                                    <div className="lines flex flex-col gap-2">
+                                        <div className="line w-15 h-1.5 bg-white"></div>
+                                        <div className="line w-10 h-1.5 bg-white"></div>
+                                        <div className="line w-5 h-1.5 bg-white"></div>
+                                    </div>
+                                    <h3 className="text-4xl leading-none -mt-2.25 text-white">
+                                        Rockstar
+                                    </h3>
+                                </div>
+                            </div>
+                            <div className="imagediv relative overflow-hidden w-full h-screen">
+                                <img
+                                    className="sky absolute top-0 left-0 w-full h-full scale-[1.5] rotate-[-20deg] object-cover"
+                                    src="./sky.png"
+                                    alt=""
+                                ></img>
+                                <img
+                                    className="bg absolute top-0 left-0 w-full h-full scale-[1.8] rotate-3 object-cover"
+                                    src="./bg.png"
+                                    alt=""
+                                ></img>
+                                <img
+                                    className="logo18 absolute bottom-[26%] left-[60%] z-20 w-[5%] scale-[1.8] rotate-[-20deg] object-cover"
+                                    src="./logo18.png"
+                                    alt=""
+                                ></img>
+                                <div className="text text-white absolute flex flex-col gap-3 top-20 left-1/2 -translate-x-1/2 scale-[1.4] rotate-[-10deg]">
+                                    <h3 className="text-[12rem] leading-none -ml-40">
+                                        grand
+                                    </h3>
+                                    <h3 className="text-[12rem] leading-none -ml-20">
+                                        theft
+                                    </h3>
+                                    <h3 className="text-[12rem] leading-none -ml-40">
+                                        auto
+                                    </h3>
+                                </div>
+                                <img
+                                    className="charater absolute -bottom-[150%] left-1/2 -translate-x-1/2 scale-[3] rotate-[-20deg]"
+                                    src="./girlbg.png"
+                                    alt=""
+                                ></img>
+
+                                <div className="btmbar absolute bottom-0 left-0 text-white w-full py-16 px-10 bg-linear-to-t from-black to-transparent">
+                                    <div className="flex gap-4 items-center">
+                                        <i className="text-4xl ri-arrow-down-line"></i>
+                                        <h3 className="text-2xl font-[Helvetica_Now_Display]">
+                                            Scroll Down
+                                        </h3>
+                                    </div>
+
+                                    <img
+                                        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-22"
+                                        src="./ps5.png"
+                                        alt=""
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Landing bottom part */}
+                        <div className="w-full h-screen flex items-center justify-center bg-black">
+                            <div className="cntnr flex text-white w-full h-[80%]">
+                                <div className="leftimg relative w-1/2 h-full">
+                                    <img
+                                        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 scale-[1.3]"
+                                        src="./imag.png"
+                                        alt=""
+                                    />
+                                </div>
+                                <div className="rightimg w-[30%] py-36">
+                                    <h1 className="text-8xl">Still Running,</h1>
+                                    <h1 className="text-8xl">Not Hunting</h1>
+                                    <p className="mt-10 text-2xl font-[Helvetica_Now_Display]">
+                                        Lorem, ipsum dolor sit amet consectetur
+                                        adipisicing elit. Laudantium quos
+                                        officiis incidunt id! Reiciendis,
+                                        laborum quibusdam obcaecati eius ullam
+                                        dicta omnis id corrupti? Pariatur et nam
+                                        assumenda dicta adipisci debitis nostrum
+                                        quasi maiores veritatis? Assumenda?
+                                    </p>
+                                    <p className="mt-3 text-2xl font-[Helvetica_Now_Display]">
+                                        Lorem, ipsum dolor sit amet consectetur
+                                        adipisicing elit. Placeat et nobis
+                                        fugit, cupiditate perspiciatis fuga
+                                        neque.
+                                    </p>
+                                    <p className="mt-3 text-2xl font-[Helvetica_Now_Display]">
+                                        Lorem ipsum, dolor sit amet consectetur
+                                        adipisicing elit. Accusantium inventore
+                                        placeat voluptates dolorem nobis ipsum,
+                                        quae maiores soluta sunt neque incidunt
+                                        saepe tempora consectetur, sit debitis.
+                                    </p>
+                                    <button className="bg-yellow-500 px-16 py-10 text-black text-4xl mt-10">
+                                        Download Now
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 }
-
-export default App
